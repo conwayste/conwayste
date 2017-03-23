fc9c9ccd03ade7610631525bc34055a502065012
Aaron Miller <aaron.miller04@gmail.com>
2017-03-23 13:08:08 -0700

    finished next but WIP (major changes to support multi-bitmaps)

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,73 @@
 use std::fmt;
+use std::collections::BTreeMap;
+
 
 /// Represents a wrapping universe in Conway's game of life.
 pub struct Universe {
-    width:        usize,           // width in u64 elements, _not_ width in cells!
-    height:       usize,
-    generation_a: Option<usize>,
-    generation_b: Option<usize>,
-    buffer_a:     Vec<Vec<u64>>,
-    buffer_b:     Vec<Vec<u64>>,
+    width:           usize,
+    height:          usize,
+    width_in_words:  usize,                     // width in u64 elements, _not_ width in cells!
+    generation:      usize,                     // current generation (1-based)
+    num_players:     usize,                     // number of players in the game (player numbers are 0-based)
+    state_index:     usize,                     // index of GenState for current generation within gen_states
+    gen_states:      Vec<GenState>,             // circular buffer
+    player_writable: Vec<Region>,               // writable region (indexed by player_id)
+}
+
+type BitGrid = Vec<Vec<u64>>;
+
+struct GenState {
+    gen_or_none:   Option<usize>,        // Some(generation number) (redundant info); if None, this is an unused buffer
+    cells:         BitGrid,              // 1 = cell is known to be Alive
+    wall_cells:    BitGrid,              // 1 = is a wall cell (should this just be fixed for the universe?)
+    known:         BitGrid,              // 1 = cell is known (always 1 if this is server)
+    player_states: Vec<PlayerGenState>,  // player-specific info (indexed by player_id)
+}
+
+struct PlayerGenState {
+    player_id: usize,
+    cells:     BitGrid,   // cells belonging to this player (if 1 here, must be 1 in GenState cells)
+    fog:       BitGrid,   // cells that the player is not allowed to know
 }
 
 
 #[derive(Eq,PartialEq,Ord,PartialOrd,Copy,Clone)]
 pub enum CellState {
     Dead,
-    Alive,              // TODO: Alive(Option<u8>) (player number)
+    Alive(Option<u8>),    // Some(player_number) or alive but not belonging to any player
     Wall,
     Fog,
 }
 
 
-#[derive(Eq,PartialEq)]
-enum WhichBuffer { A, B }
+impl CellState {
+    // Roughly follows RLE specification: http://www.conwaylife.com/wiki/Run_Length_Encoded
+    pub fn to_char(self) -> char {
+        match self {
+            CellState::Alive(Some(player_id)) => {
+                if player_id >= 23 {
+                    panic!("Player IDs must be less than 23 to be converted to chars");
+                }
+                char::from_u32(player_id as u32 + 65).unwrap()
+            }
+            CellState::Alive(None) => 'o',
+            CellState::Dead        => 'b',
+            CellState::Wall        => 'W',
+            CellState::Fog         => '?',
+        }
+    }
+}
+
+
+fn new_bitgrid(width_in_words: usize, height: usize) -> BitGrid {
+    let mut result: BitGrid = Vec::new();
+    for _ in 0 .. height {
+        let row: Vec<u64> = vec![0; width_in_words];
+        result.push(row);
+    }
+    result
+}
+
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -46,12 +92,11 @@ impl fmt::Display for Universe {
 }
 
 
+// TODO: unit tests
 impl Universe {
     // sets the state of a cell
-    // TODO: unit tests
     // TODO: when multiple bitmaps are supported, adjust this
     pub fn set(&mut self, col: usize, row: usize, cell: CellState) {
-        //XXX bounds checks
         let latest = self.latest();
         let buffer_cur = if latest == WhichBuffer::A { &mut self.buffer_a } else { &mut self.buffer_b };
         let word_col = col/64;
@@ -72,9 +117,7 @@ impl Universe {
 
     // switches any non-dead state to CellState::Dead
     // switches CellState::Dead to CellState::Alive
-    // TODO: unit tests
     pub fn toggle(&mut self, col: usize, row: usize) -> CellState {
-        //XXX bounds checks
         let latest = self.latest();
         let buffer_cur = if latest == WhichBuffer::A { &mut self.buffer_a } else { &mut self.buffer_b };
         let word_col = col/64;
@@ -93,43 +136,65 @@ impl Universe {
 
     /// Instantiate a new blank universe with the given width and height, in cells.
     /// The universe is at generation 1.
-    pub fn new(width: usize, height: usize) -> Result<Universe, &'static str> {
+    pub fn new(width:           usize,
+               height:          usize,
+               is_server:       bool,
+               history:         usize,
+               num_players:     usize,
+               player_writable: Vec<Region>) -> Result<Universe, &'static str> {
         if height == 0 {
             return Err("Height must be positive");
         }
-
-        if width != width/64*64 {
+        let width_in_words = width/64;
+        if width != width_in_words * 64 {
             return Err("Width must be a multiple of 64");
         } else if width == 0 {
             return Err("Width must be positive");
         }
 
-        let mut buffer_a: Vec<Vec<u64>> = Vec::new();
-        for _ in 0 .. height {
-            let row: Vec<u64> = vec![0; width/64];
-            buffer_a.push(row);
-        }
-
-        let mut buffer_b: Vec<Vec<u64>> = Vec::new();
-        for _ in 0 .. height {
-            let row: Vec<u64> = vec![0; width/64];
-            buffer_b.push(row);
+        let mut gen_states = Vec::new();
+        for i in 0 .. history {
+            let mut player_states = Vec::new();
+            for player_id in 0 .. num_players {
+                player_states.push(PlayerGenState {
+                    player_id: player_id,
+                    cells:     new_bitgrid(width_in_words, height),
+                    fog:       new_bitgrid(width_in_words, height),
+                });
+            }
+            let mut known = new_bitgrid(width_in_words, height);
+            if is_server && i == 0 {
+                for y in 0 .. height {
+                    for x in 0 .. width_in_words {
+                        known[y][x] = u64::max_value();   // if server, all cells are known
+                    }
+                }
+            }
+            gen_states.push(GenState {
+                gen_or_none:   if i == 0 { Some(1) } else { None },
+                cells:         new_bitgrid(width_in_words, height),
+                wall_cells:    new_bitgrid(width_in_words, height),
+                known:         known,
+                player_states: player_states,
+            });
         }
 
         Ok(Universe {
-            width:  width/64,
-            height: height,
-            generation_a: Some(1),
-            generation_b: None,
-            buffer_a: buffer_a,
-            buffer_b: buffer_b,
+            width:           width,
+            height:          height,
+            width_in_words:  width_in_words,
+            generation:      1,
+            num_players:     num_players,
+            state_index:     0,
+            gen_states:      gen_states,
+            player_writable: player_writable,
         })
     }
 
 
     /// Return width in cells.
     pub fn width(&self) -> usize {
-        return self.width * 64;
+        return self.width;
     }
 
 
@@ -139,34 +204,9 @@ impl Universe {
     }
 
 
-    fn latest(&self) -> WhichBuffer {
-        if let Some(gen_a) = self.generation_a {
-            match self.generation_b {
-                Some(gen_b) => {
-                    if gen_a < gen_b {
-                        assert!(gen_a + 1 == gen_b);
-                        WhichBuffer::B
-                    } else if gen_a > gen_b {
-                        assert!(gen_a - 1 == gen_b);
-                        WhichBuffer::A
-                    } else {
-                        panic!("The generations are equal")
-                    }
-                },
-                None => WhichBuffer::A
-            }
-        } else {
-            assert!(self.generation_b.is_some());
-            WhichBuffer::B
-        }
-    }
-
     /// Get the latest generation number (1-based).
     pub fn latest_gen(&self) -> usize {
-        match self.latest() {
-            WhichBuffer::A => self.generation_a.unwrap(),
-            WhichBuffer::B => self.generation_b.unwrap()
-        }
+        self.generation
     }
 
     fn next_single_gen(nw: u64, n: u64, ne: u64, w: u64, center: u64, e: u64, sw: u64, s: u64, se: u64) -> u64 {
@@ -207,56 +247,160 @@ impl Universe {
         !y1&y6&(y2&int1&y5 | y4&!y5) | y1&int1&(!y2&(y5 | y6) | y2&!y5) | !y1&y4&(y2^y5)
     }
 
+    /*
+     * A B C
+     * D   E
+     * F G H
+     */
+    // a cell is 0 if itself or any of its neighbors are 0
+    fn contagious_zero(nw: u64, n: u64, ne: u64, w: u64, center: u64, e: u64, sw: u64, s: u64, se: u64) -> u64 {
+        let a  = (nw     << 63) | (n      >>  1);
+        let b  =  n;
+        let c  = (n      <<  1) | (ne     >> 63);
+        let d  = (w      << 63) | (center >> 1);
+        let e  = (center <<  1) | (e      >> 63);
+        let f  = (sw     << 63) | (s      >>  1);
+        let g  =  s;
+        let h  = (s      <<  1) | (se     >> 63);
+        a & b & c & d & center & e & f & g & h
+    }
+
+
+    // a cell is 1 if itself or any of its neighbors are 1
+    fn contagious_one(nw: u64, n: u64, ne: u64, w: u64, center: u64, e: u64, sw: u64, s: u64, se: u64) -> u64 {
+        let a  = (nw     << 63) | (n      >>  1);
+        let b  =  n;
+        let c  = (n      <<  1) | (ne     >> 63);
+        let d  = (w      << 63) | (center >> 1);
+        let e  = (center <<  1) | (e      >> 63);
+        let f  = (sw     << 63) | (s      >>  1);
+        let g  =  s;
+        let h  = (s      <<  1) | (se     >> 63);
+        a | b | c | d | center | e | f | g | h
+    }
+
+
     /// Compute the next generation. Returns the new latest generation number.
+    // TODO: write some good unit tests covering all features and cases, then optimize & rewrite (use macros?)
     pub fn next(&mut self) -> usize {
-        let latest     = self.latest();
-        let latest_gen = self.latest_gen();
-        let buffer_cur;
-        let buffer_next;
-        if latest == WhichBuffer::A {
-            buffer_cur  =     &self.buffer_a;
-            buffer_next = &mut self.buffer_b;
-        } else {
-            buffer_cur  =     &self.buffer_b;
-            buffer_next = &mut self.buffer_a;
+        // get the buffers and buffers_next
+        assert!(self.gen_states[self.state_index].gen_or_none.unwrap() == self.generation);
+        let next_state_index = (self.state_index + 1) % self.gen_states.len();
+        let cells      = &self.gen_states[self.state_index].cells;
+        let wall       = &self.gen_states[self.state_index].wall_cells;
+        let known      = &self.gen_states[self.state_index].known;
+        let cells_next = &mut self.gen_states[next_state_index].cells;
+        let wall_next  = &mut self.gen_states[next_state_index].wall_cells;
+        let known_next = &mut self.gen_states[next_state_index].known;
+
+        let mut player_cells_vec      = Vec::with_capacity(self.num_players);
+        let mut player_fog_vec        = Vec::with_capacity(self.num_players);
+        let mut player_cells_next_vec = Vec::with_capacity(self.num_players);
+        let mut player_fog_next_vec   = Vec::with_capacity(self.num_players);
+        for player_id in 0 .. self.num_players {
+            player_cells_vec.push(&self.gen_states[self.state_index].player_states[player_id].cells);
+            player_fog_vec.push(&self.gen_states[self.state_index].player_states[player_id].fog);
+            player_cells_next_vec.push(&self.gen_states[next_state_index].player_states[player_id].cells);
+            player_fog_next_vec.push(&self.gen_states[next_state_index].player_states[player_id].fog);
         }
+
         for row_idx in 0 .. self.height {
-            let row_n = &buffer_cur[(row_idx + self.height - 1) % self.height];
-            let row_c = &buffer_cur[ row_idx ];
-            let row_s = &buffer_cur[(row_idx + 1) % self.height];
+            let n_row_idx = (row_idx + self.height - 1) % self.height;
+            let s_row_idx = (row_idx + 1) % self.height;
+            let cells_row_n = &cells[n_row_idx];
+            let cells_row_c = &cells[ row_idx ];
+            let cells_row_s = &cells[s_row_idx];
+            let wall_row_c  = &wall[ row_idx ];
+            let known_row_n = &known[n_row_idx];
+            let known_row_c = &known[ row_idx ];
+            let known_row_s = &known[s_row_idx];
+
             // These will be shifted over at the beginning of the loop
-            let mut nw;
-            let mut w;
-            let mut sw;
-            let mut n   = row_n[self.width - 1];
-            let mut cen = row_c[self.width - 1];
-            let mut s   = row_s[self.width - 1];
-            let mut ne  = row_n[0];
-            let mut e   = row_c[0];
-            let mut se  = row_s[0];
-            for col_idx in 0 .. self.width {
+            let mut cells_nw;
+            let mut cells_w;
+            let mut cells_sw;
+            let mut cells_n   = cells_row_n[self.width_in_words - 1];
+            let mut cells_cen = cells_row_c[self.width_in_words - 1];
+            let mut cells_s   = cells_row_s[self.width_in_words - 1];
+            let mut cells_ne  = cells_row_n[0];
+            let mut cells_e   = cells_row_c[0];
+            let mut cells_se  = cells_row_s[0];
+            let mut known_nw;
+            let mut known_w;
+            let mut known_sw;
+            let mut known_n   = known_row_n[self.width_in_words - 1];
+            let mut known_cen = known_row_c[self.width_in_words - 1];
+            let mut known_s   = known_row_s[self.width_in_words - 1];
+            let mut known_ne  = known_row_n[0];
+            let mut known_e   = known_row_c[0];
+            let mut known_se  = known_row_s[0];
+
+            for col_idx in 0 .. self.width_in_words {
                 // shift over
-                nw  = n;
-                n   = ne;
-                w   = cen;
-                cen = e;
-                sw  = s;
-                s   = se;
-                ne  = row_n[(col_idx + 1) % self.width];
-                e   = row_c[(col_idx + 1) % self.width];
-                se  = row_s[(col_idx + 1) % self.width];
-                let result = Universe::next_single_gen(nw, n, ne, w, cen, e, sw, s, se);
+                cells_nw  = cells_n;
+                cells_n   = cells_ne;
+                cells_w   = cells_cen;
+                cells_cen = cells_e;
+                cells_sw  = cells_s;
+                cells_s   = cells_se;
+                cells_ne  = cells_row_n[(col_idx + 1) % self.width_in_words];
+                cells_e   = cells_row_c[(col_idx + 1) % self.width_in_words];
+                cells_se  = cells_row_s[(col_idx + 1) % self.width_in_words];
+                known_nw  = known_n;
+                known_n   = known_ne;
+                known_w   = known_cen;
+                known_cen = known_e;
+                known_sw  = known_s;
+                known_s   = known_se;
+                known_ne  = known_row_n[(col_idx + 1) % self.width_in_words];
+                known_e   = known_row_c[(col_idx + 1) % self.width_in_words];
+                known_se  = known_row_s[(col_idx + 1) % self.width_in_words];
+
+                // apply BitGrid changes
+                let mut cells_cen_next = Universe::next_single_gen(cells_nw, cells_n, cells_ne, cells_w, cells_cen, cells_e, cells_sw, cells_s, cells_se);
+                known_next[row_idx][col_idx] = Universe::contagious_zero(known_nw, known_n, known_ne, known_w, known_cen, known_e, known_sw, known_s, known_se);
+
+                cells_cen_next &= known_next[row_idx][col_idx];
+                cells_cen_next &= wall_row_c[col_idx];
 
                 // assign to the u64 element in the next generation
-                buffer_next[row_idx][col_idx] = result;
+                cells_next[row_idx][col_idx] = cells_cen_next;
+
+                let mut in_multiple: u64 = 0;
+                let mut seen_before: u64 = 0;
+                for player_id in 0 .. self.num_players {
+                    let player_cell_next =
+                        Universe::contagious_one(
+                            player_cells_vec[player_id][n_row_idx][(col_idx + self.width_in_words - 1) % self.width_in_words],
+                            player_cells_vec[player_id][n_row_idx][col_idx],
+                            player_cells_vec[player_id][n_row_idx][(col_idx + 1) % self.width_in_words],
+                            player_cells_vec[player_id][ row_idx ][(col_idx + self.width_in_words - 1) % self.width_in_words],
+                            player_cells_vec[player_id][ row_idx ][col_idx],
+                            player_cells_vec[player_id][ row_idx ][(col_idx + 1) % self.width_in_words],
+                            player_cells_vec[player_id][s_row_idx][(col_idx + self.width_in_words - 1) % self.width_in_words],
+                            player_cells_vec[player_id][s_row_idx][col_idx],
+                            player_cells_vec[player_id][s_row_idx][(col_idx + 1) % self.width_in_words]
+                        ) & cells_cen_next;
+                    in_multiple |= player_cell_next & seen_before;
+                    seen_before |= player_cell_next;
+                    player_cells_next_vec[player_id][row_idx][col_idx] = player_cell_next;
+                }
+                for player_id in 0 .. self.num_players {
+                    let mut cell_next = player_cells_next_vec[player_id][row_idx][col_idx];
+                    cell_next &= !in_multiple; // if a cell would have belonged to multiple players, it belongs to none
+                    player_fog_next_vec[player_id][row_idx][col_idx] = player_fog_vec[player_id][row_idx][col_idx] & !cell_next; // clear fog!
+                    player_cells_next_vec[player_id][row_idx][col_idx] = cell_next;
+                }
             }
+
+            // copy wall to wall_next
+            wall_next[row_idx].copy_from_slice(wall_row_c);
         }
-        let new_latest_gen = latest_gen + 1;
-        if latest == WhichBuffer::A {
-            self.generation_b = Some(new_latest_gen);
-        } else {
-            self.generation_a = Some(new_latest_gen);
-        }
+
+        // increment generation in appropriate places
+        self.generation += 1;
+        self.state_index = next_state_index;
+        self.gen_states[next_state_index].gen_or_none = Some(self.generation);
         new_latest_gen
     }
 
@@ -300,7 +444,7 @@ impl Universe {
 
     /// Get a Region of the same size as the universe
     pub fn region(&self) -> Region {
-        Region::new(0, 0, self.width*64, self.height)
+        Region::new(0, 0, self.width, self.height)
     }
 }
 
