aa183e88a3ee987ea250f96d6409a0243865e312
Aaron Miller <aaron.miller04@gmail.com>
2017-10-14 20:44:36 -0700

    use bincode and PlayerPacket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate futures;
 extern crate env_logger;
 extern crate tokio_core;
+#[macro_use]
+extern crate serde_derive;
+extern crate bincode;
 
 use std::io;
 use std::net::SocketAddr;
@@ -9,19 +12,35 @@ use std::str;
 use futures::{Future, Stream, Sink};
 use tokio_core::net::{UdpSocket, UdpCodec};
 use tokio_core::reactor::Core;
+use bincode::{serialize, deserialize, Infinite};
 
-pub struct LineCodec;
+struct LineCodec;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+enum Action {
+    Click,
+    Delete,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct PlayerPacket {
+    player_name: String,
+    number:      u64,
+    action:      Action,
+}
 
 impl UdpCodec for LineCodec {
-    type In = (SocketAddr, Vec<u8>);
-    type Out = (SocketAddr, Vec<u8>);
+    type In = (SocketAddr, PlayerPacket);
+    type Out = (SocketAddr, PlayerPacket);
 
     fn decode(&mut self, addr: &SocketAddr, buf: &[u8]) -> io::Result<Self::In> {
-        Ok((*addr, buf.to_vec()))
+        let decoded: PlayerPacket = deserialize(buf).unwrap();
+        Ok((*addr, decoded))
     }
 
-    fn encode(&mut self, (addr, buf): Self::Out, into: &mut Vec<u8>) -> SocketAddr {
-        into.extend(buf);
+    fn encode(&mut self, (addr, player_packet): Self::Out, into: &mut Vec<u8>) -> SocketAddr {
+        let encoded: Vec<u8> = serialize(&player_packet, Infinite).unwrap();
+        into.extend(encoded);
         addr
     }
 }
@@ -47,20 +66,35 @@ fn main() {
     // Start off by sending a ping from a to b, afterwards we just print out
     // what they send us and continually send pings
     // let pings = stream::iter((0..5).map(Ok));
-    let a = a_sink.send((b_addr, b"PING".to_vec())).and_then(|a_sink| {
+    let first_packet = PlayerPacket {
+        player_name: String::from("me"),
+        number:      1234,
+        action:      Action::Click,
+    };
+    let a = a_sink.send((b_addr, first_packet)).and_then(|a_sink| {
         let mut i = 0;
-        let new_a_stream = a_stream.take(4).map(move |(addr, msg)| {
+        let new_a_stream = a_stream.take(4).map(move |(addr, packet)| {
             i += 1;
-            println!("[a] i={} recv: {}", i, String::from_utf8_lossy(&msg));
-            (addr, b"PONG".to_vec())
+            println!("[a] i={} recv: {:?}", i, packet);
+            let out_packet = PlayerPacket {
+                player_name: String::from("you"),
+                number:      5678,
+                action:      Action::Delete,
+            };
+            (addr, out_packet)
         });
         a_sink.send_all(new_a_stream)
     });
 
     // The second client we have will receive the pings from `a` and then send back pongs.
-    let new_b_stream = b_stream.map(|(addr, msg)| {
-        println!("[b].recv: {}", String::from_utf8_lossy(&msg));
-        (addr, b"PONG".to_vec())
+    let new_b_stream = b_stream.map(|(addr, packet)| {
+        println!("[b].recv: {:?}", packet);
+        let another_out_packet = PlayerPacket {
+            player_name: String::from("idk"),
+            number:      8765,
+            action:      Action::Click,
+        };
+        (addr, another_out_packet)
     });
     let b = b_sink.send_all(new_b_stream);
 
