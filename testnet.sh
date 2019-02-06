16a938e7807be9b49ed73e972b9c3fd408eaeef9
manghi <manghirs@gmail.com>
2019-02-05 21:09:27 -0800

    minor style updates

--- testnet.sh
+++ testnet.sh
@@ -3,6 +3,8 @@
 # Conwayste Developers © 2018
 #
 
+CLIENTLIST=( client )
+
 # $1: client or server
 # $2: command to send
 function send()
@@ -39,7 +41,6 @@ function tmuxkill()
 function ConnectToServerDefaultTestRoom()
 {
     send $1 "/connect Test$1"
-    sleep 5
     send $1 "/new TestRoom"
     send $1 "/join TestRoom"
 }
@@ -85,15 +86,15 @@ function PrintStats()
 
 function EnableTrafficControl()
 {
-# Impose arbitrary latency to outbound UDP
-#
-# https://serverfault.com/questions/336089/adding-latency-to-outbound-udp-packets-with-tc
-# http://luxik.cdi.cz/~devik/qos/htb/manual/userg.htm
-# https://wiki.linuxfoundation.org/networking/netem?s[]=netem
-# https://gist.github.com/keturn/541339
-# http://home.ifi.uio.no/paalh/students/AndersMoe.pdf
+    # Impose arbitrary latency to outbound UDP
+    #
+    # https://serverfault.com/questions/336089/adding-latency-to-outbound-udp-packets-with-tc
+    # http://luxik.cdi.cz/~devik/qos/htb/manual/userg.htm
+    # https://wiki.linuxfoundation.org/networking/netem?s[]=netem
+    # https://gist.github.com/keturn/541339
+    # http://home.ifi.uio.no/paalh/students/AndersMoe.pdf
 
-#sudo tc qdisc add dev eth0 root netem delay 200ms 40ms 25% loss 15.3% 25% duplicate 1% corrupt 0.1% reorder 5% 50%
+    #sudo tc qdisc add dev eth0 root netem delay 200ms 40ms 25% loss 15.3% 25% duplicate 1% corrupt 0.1% reorder 5% 50%
 
     sudo tc qdisc add dev lo root handle 1: prio
     sudo tc qdisc add dev lo parent 1:3 handle 30: netem delay 200ms 40ms 25% loss 15.3% 25% duplicate 1% corrupt 0.1% reorder 5% 50%
@@ -107,60 +108,67 @@ function DisableTrafficControl()
     sudo tc qdisc del dev lo root
 }
 
-CLIENTLIST=( client )
+function main()
+{
+    DisableTrafficControl # Clean up from a previous session if needed
+    tmuxkill
+    cargo build
 
-DisableTrafficControl # Clean up from a previous session if needed
-tmuxkill
-cargo build
-
-tmux new-session -d -s server "export RUST_BACKTRACE=1; export RUST_LOG=server; ./target/debug/server; read"
-for i in ${CLIENTLIST[@]}; do
-    printf "Starting client $i"
-    tmux new-session -d -s $i "export RUST_BACKTRACE=1; export RUST_LOG=client; ./target/debug/client; read"
-done
-
-echo "Listing sessions..."
-tmux ls
-
-echo "Launching Terminal Tmux Windows..."
-if detect_kde;
-then
+    tmux new-session -d -s server "export RUST_BACKTRACE=1; export RUST_LOG=server; ./target/debug/server; read"
     for i in ${CLIENTLIST[@]}; do
-        konsole --noclose -e tmux attach-session -t $i &
+        printf "Starting client $i"
+        tmux new-session -d -s $i "export RUST_BACKTRACE=1; export RUST_LOG=client; ./target/debug/client; read"
     done
-    konsole --noclose -e tmux attach-session -t server &
-else
-# For others environments swap in your favorite terminal
-    echo "Your terminal environment isn't specified. Open me up and add support."
-    exit 0
-fi
 
-echo "Attached to tmux-sessions."
-for i in ${CLIENTLIST[@]}; do
-    ConnectToServerDefaultTestRoom $i
-done
+    echo "Listing sessions..."
+    tmux ls
 
-echo "Enabling Traffic Control..."
-EnableTrafficControl
+    echo "Launching Terminal Tmux Windows..."
+    if detect_kde;
+    then
+        for i in ${CLIENTLIST[@]}; do
+            konsole --noclose -e tmux attach-session -t $i &
+        done
+        konsole --noclose -e tmux attach-session -t server &
+    else
+    # For others environments swap in your favorite terminal
+        echo "Your terminal environment isn't specified. Open me up and add support."
+        exit 0
+    fi
 
-echo "Basic test: Spam 500 /list and chat messages"
-# First basic test... spam 500 /list and chat messages
-roomCmdCount=1000
-until [[ $roomCmdCount -eq 0 ]];
-do
+    echo "Attached to tmux-sessions."
     for i in ${CLIENTLIST[@]}; do
-        IssueInRoomCommands $i
+        ConnectToServerDefaultTestRoom $i
     done
-    roomCmdCount=$((roomCmdCount-1))
-done
-echo "....Done."
 
-echo "Wait to disable Traffic Control..."
-sleep 10
-echo "Disabling Traffic Control..."
-DisableTrafficControl
+    # Delay to start tmux logging
+    sleep 10
 
-PrintStats
-LeaveRooms
-#Disconnect
+    echo "Enabling Traffic Control..."
+    EnableTrafficControl
 
+    # First basic test... spam 500 /list and chat messages
+    roomCmdCount=1
+
+    echo "Basic test: Spam $roomCmdCount /list and chat messages"
+
+    until [[ $roomCmdCount -eq 0 ]];
+    do
+        for i in ${CLIENTLIST[@]}; do
+            IssueInRoomCommands $i
+        done
+        roomCmdCount=$((roomCmdCount-1))
+    done
+    echo "....Done."
+
+    echo "Wait to disable Traffic Control..."
+    sleep 10
+    echo "Disabling Traffic Control..."
+    DisableTrafficControl
+
+    PrintStats
+    LeaveRooms
+    #Disconnect
+}
+
+main
