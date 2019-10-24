09b22c44ea0df77b2d335381a6892bb0d4a6815f
manghi <manghirs@gmail.com>
2019-10-23 21:01:21 -0700

    Added macro to behave like Go's type-switch, but specific to Results<T,E>

--- conwayste/src/error.rs
+++ conwayste/src/error.rs
@@ -15,34 +15,67 @@
  *  You should have received a copy of the GNU General Public License
  *  along with libconway.  If not, see <http://www.gnu.org/licenses/>. */
 
+/// A macro inspired by Go's Type Switch
+///
+/// [See the Go documentation for additional details.](https://tour.golang.org/methods/16])
+///
+/// It accepts a `Result<T, E>` where `E` must be `dyn Error`. The macro attempts to downcast the result
+/// into one or more errors as specified in the "match" expressions. If no downcasts are successful,
+/// then the macro simply passes along the original result without modification.
+///
+/// # Usage
+///     handle_error!(input_result, [custom_return_type, ]
+///         [Error(_) => { ...},]*      // matcher
+///     );
+///
+/// # Arguments
+///     input_result - a result type
+///     custom_return_type - (optional) specify a type T for Ok(T); Default type is ()
+///     matcher - (optional) an error which implements Error
+///
+/// # Examples
+/// ```rust
+/// // See the Error documentation page for the SuperError and SuperSideKickError definitions
+///
+/// fn give_me_an_error_result() -> Result<(), Box<dyn Error>> {
+///     Err(Box::new(SuperError{}))
+/// }
+///
+/// let a_result = give_me_an_error_result();
+/// handle_error!(a_result,
+///     SuperError(e) => {
+///         println!("I got a SuperError after the downcast");
+///     },
+///     SuperSideKickError(e) => {
+///         println!("I got a SuperSideKickError after the downcast!");
+///     },
+/// );
+/// ```
+///
+#[macro_export]
+macro_rules! handle_error {
+   ($input:ident $(, $matcher:ident ($var:ident) => $result:expr)*) => {
+        $(
+            let $input = $input.or_else(|boxed_error| -> Result<(), Box<dyn Error>> {
+            boxed_error.downcast::<$matcher>()
+                .and_then(|$var| $result)
+            });
+        )*
+   };
+
+   ($input:ident, $type:ty $(, $matcher:ident ($var:ident) => $result:expr)*) => {
+        $(
+            let $input = $input.or_else(|boxed_error| -> Result<$type, Box<dyn Error>> {
+            boxed_error.downcast::<$matcher>()
+                .and_then(|$var| $result)
+            });
+        )*
+   };
+}
+
 custom_error! {pub ConwaysteError
     InvalidUIAction {reason: String} = "ConwaysteError->InvalidUIAction->{reason}",
     NoAssociatedUIAction{reason: String} = "ConwaysteError->NoAssociatedUIAction->{reason}"
 }
 
 pub type ConwaysteResult<T> = ::std::result::Result<T, ConwaysteError>;
-
-/*
-
-impl PartialEq for ConwaysteError {
-    fn eq(&self, other: &ConwaysteError) -> bool {
-        use ConwaysteError::*;
-        match *self {
-            InvalidUIAction{reason: ref self_reason} => {
-                if let InvalidUIAction{reason: ref other_reason} = *other {
-                    self_reason == other_reason
-                } else {
-                    false
-                }
-            }
-            NoAssociatedUIAction{reason: ref self_reason} => {
-                if let NoAssociatedUIAction{reason: ref other_reason} = *other {
-                    self_reason == other_reason
-                } else {
-                    false
-                }
-            }
-        }
-    }
-}
-*/
