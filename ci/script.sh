c34dedd03558d7bcc82759f4b256285332854e86
Aaron Miller <aaron@iforgotmy.name>
2020-02-18 07:51:40 -0800

    CI: try to get appveyor and travis configs to work

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,12 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
+    # TODO: verify this works
+    if which apt-get > /dev/null; then
+        # this is Ubuntu
+        sudo -E apt-get -yq --no-install-suggests --no-install-recommends $(travis_apt_get_options) install libudev-dev libasound2-dev
+    fi
+
     cross build --target $TARGET
     cross build --target $TARGET --release
 
@@ -13,12 +19,11 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
 if [ -z $TRAVIS_TAG ]; then
     main
 fi
+
+# vim:set ts=4 sw=4 softtabstop=4 et:
