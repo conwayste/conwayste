3159b3ca0a17c6b59d975a5fba2588e9a43df465
manghi <manghirs@gmail.com>
2020-02-24 20:34:45 -0800

    Updated installation instructions for general use
    - Updated to Wireshark 3.2
    - Using pkg-config usage to probe library headers
    - Added install/setup instructions

--- dissect-netwayste/build.rs
+++ dissect-netwayste/build.rs
@@ -1,14 +1,17 @@
 extern crate bindgen;
+extern crate pkg_config;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    pkg_config::Config::new().atleast_version("2.0").probe("glib-2.0").unwrap();
+    pkg_config::Config::new().probe("wireshark").unwrap();
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I../../wireshark") //XXX XXX XXX do something about this!
-        .clang_arg("-I/usr/include/glib-2.0") //XXX XXX XXX this is not that great either
-        .clang_arg("-I/usr/lib64/glib-2.0/include") //XXX XXX XXX nor this
+        .clang_arg("-I/usr/include/wireshark")
+        .clang_arg("-I/usr/include/glib-2.0")
+        .clang_arg("-I/usr/lib/x86_64-linux-gnu/glib-2.0/include") //XXX XXX XXX nor this  // get from the pkg config command
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
