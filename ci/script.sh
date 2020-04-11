577822385e5a4ba76310a4af5b35ea66246d0a0c
manghi <manghirs@gmail.com>
2020-04-10 17:14:11 -0700

    Disable dissector win builds in Travis maybe?

--- ci/script.sh
+++ ci/script.sh
@@ -5,15 +5,25 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     # XXX run in docker: sudo apt-get -yq --no-install-suggests --no-install-recommends install libudev-dev libasound2-dev
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    if [ "$TARGET" =~ .*"windows".* ]; then
+        cross build --target $TARGET --workspace --exclude dissect-netwayste
+        cross build --target $TARGET --release --workspace --exclude dissect-netwayste
+    else
+        cross build --target $TARGET
+        cross build --target $TARGET --release
+    fi
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    if [ "$TARGET" =~ .*"windows".* ]; then
+        cross test --target $TARGET --workspace --exclude dissect-netwayste
+        cross test --target $TARGET --release --workspace --exclude dissect-netwayste
+    else
+        cross test --target $TARGET
+        cross test --target $TARGET --release
+    fi
 }
 
 # we don't run the "test phase" when doing deploys
