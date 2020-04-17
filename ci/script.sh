975dc9325c447b987c17e083116f603905bb44ab
Aaron Miller <aaron@iforgotmy.name>
2020-04-16 22:11:36 -0700

    CI: fix bash script errors

--- ci/script.sh
+++ ci/script.sh
@@ -5,7 +5,7 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     # XXX run in docker: sudo apt-get -yq --no-install-suggests --no-install-recommends install libudev-dev libasound2-dev
-    if [ "$TARGET" =~ .*"windows".* ]; then
+    if [[ "$TARGET" = *windows* ]]; then
         cross build --target $TARGET --workspace --exclude dissect-netwayste
         cross build --target $TARGET --release --workspace --exclude dissect-netwayste
     else
@@ -17,7 +17,7 @@ main() {
         return
     fi
 
-    if [ "$TARGET" =~ .*"windows".* ]; then
+    if [[ "$TARGET" = *windows* ]]; then
         cross test --target $TARGET --workspace --exclude dissect-netwayste
         cross test --target $TARGET --release --workspace --exclude dissect-netwayste
     else
