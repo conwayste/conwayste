conwayste/conwayste

commit fe762e635c813679e5fc3d75c75b97f50fa3e286

[DNC] Moving universe and running player inputs into game area

conwayste/src/ui/gamearea.rs
@@ -17,48 +17,98 @@
  *  <http://www.gnu.org/licenses/>. */
 
 use super::{
-    context::{EmitEvent, Event, EventType, Handled, HandlerData, UIContext},
+    context::{EmitEvent, Event, EventType, Handled, HandlerData, KeyCodeOrChar, UIContext},
     widget::Widget,
     UIError, UIResult,
 };
+use crate::{config::Config, constants::*};
+use conway::{
+    error::ConwayError,
+    grids::{BitGrid, CharGrid, Rotation},
+    rle::Pattern,
+    universe::{BigBang, CellState, PlayerBuilder, Region, Universe},
+    ConwayResult,
+};
 use ggez::graphics::Rect;
+use ggez::input::keyboard::KeyCode;
 use ggez::nalgebra::{Point2, Vector2};
 use ggez::{Context, GameResult};
 use id_tree::NodeId;
 use std::error::Error;
+use std::fmt;
 
-#[derive(Debug)]
 pub struct GameArea {
     id:                     Option<NodeId>,
     pub has_keyboard_focus: bool,
     z_index:                usize,
     dimensions:             Rect,
     handler_data:           HandlerData,
+    pub uni:                Universe,
+    game_state:             GameAreaState,
+}
+
+impl fmt::Debug for GameArea {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("GameArea")
+            .field("id", &self.id)
+            .field("has_keyboard_focus", &self.has_keyboard_focus)
+            .finish()
+    }
 }
 
 /// For now, this is a dummy widget to represent the actual game area. It may not always be a dummy
 /// widget.
 impl GameArea {
     pub fn new() -> Self {
+        let bigbang = {
+            // we're going to have to tear this all out when this becomes a real game
+            let player0_writable = Region::new(100, 70, 34, 16);
+            let player1_writable = Region::new(0, 0, 80, 80);
+
+            let player0 = PlayerBuilder::new(player0_writable);
+            let player1 = PlayerBuilder::new(player1_writable);
+            let players = vec![player0, player1];
+
+            BigBang::new()
+                .width(universe_width_in_cells)
+                .height(universe_height_in_cells)
+                .server_mode(true) // TODO will change to false once we get server support up
+                // Currently 'client' is technically both client and server
+                .history(HISTORY_SIZE)
+                .fog_radius(FOG_RADIUS)
+                .add_players(players)
+                .birth()
+        };
+        let uni = bigbang.unwrap();
+
+        init_patterns(&mut uni).unwrap();
+
         let mut game_area = GameArea {
             id:                 None,
             has_keyboard_focus: false,
             z_index:            0,
             dimensions:         Rect::default(),
             handler_data:       HandlerData::new(),
+            uni:                uni,
+            game_state:         GameAreaState::default(),
         };
 
         // Set handlers for toggling has_keyboard_focus
         let gain_focus_handler =
-            move |obj: &mut dyn EmitEvent, _uictx: &mut UIContext, _evt: &Event| -> Result<Handled, Box<dyn Error>> {
+            move |obj: &mut dyn EmitEvent, _uictx: &mut UIContext, evt: &Event| -> Result<Handled, Box<dyn Error>> {
                 let game_area = obj.downcast_mut::<GameArea>().unwrap(); // unwrap OK
-                game_area.has_keyboard_focus = true;
+                println!("Game area: Gained Focus");
+                if evt.is_key_event() {
+                    game_area.has_keyboard_focus = true;
+                }
+
                 Ok(Handled::NotHandled)
             };
 
         let lose_focus_handler =
             move |obj: &mut dyn EmitEvent, _uictx: &mut UIContext, _evt: &Event| -> Result<Handled, Box<dyn Error>> {
                 let game_area = obj.downcast_mut::<GameArea>().unwrap(); // unwrap OK
+                println!("Game area: Lost Focus");
                 game_area.has_keyboard_focus = false;
                 Ok(Handled::NotHandled)
             };
@@ -70,8 +120,309 @@ impl GameArea {
             .on(EventType::LoseFocus, Box::new(lose_focus_handler))
             .unwrap(); // unwrap OK
 
+        game_area.on(EventType::Update, Box::new(update_handler)).unwrap(); // unwrap OK
+
+        game_area.on(EventType::KeyPress, Box::new(keypress_handler)).unwrap();
+        game_area.on(EventType::Click, Box::new(mouse_handler)).unwrap();
+
         game_area
     }
+}
+
+fn init_patterns(uni: &mut Universe) -> ConwayResult<()> {
+    let _pat = Pattern("10$10b16W$10bW14bW$10bW14bW$10bW14bW$10bW14bW$10bW14bW$10bW14bW$10bW14bW$10bW14bW$10bW$10bW$10bW$10b16W48$100b2A5b2A$100b2A5b2A2$104b2A$104b2A5$122b2Ab2A$121bA5bA$121bA6bA2b2A$121b3A3bA3b2A$126bA!".to_owned());
+
+    // Simkin glider gun
+    uni.toggle(100, 70, 0)?;
+    uni.toggle(100, 71, 0)?;
+    uni.toggle(101, 70, 0)?;
+    uni.toggle(101, 71, 0)?;
+
+    uni.toggle(104, 73, 0)?;
+    uni.toggle(104, 74, 0)?;
+    uni.toggle(105, 73, 0)?;
+    uni.toggle(105, 74, 0)?;
+
+    uni.toggle(107, 70, 0)?;
+    uni.toggle(107, 71, 0)?;
+    uni.toggle(108, 70, 0)?;
+    uni.toggle(108, 71, 0)?;
+
+    /* eater
+    uni.toggle(120, 87, 0)?;
+    uni.toggle(120, 88, 0)?;
+    uni.toggle(121, 87, 0)?;
+    uni.toggle(121, 89, 0)?;
+    uni.toggle(122, 89, 0)?;
+    uni.toggle(123, 89, 0)?;
+    uni.toggle(123, 90, 0)?;
+    */
+
+    uni.toggle(121, 80, 0)?;
+    uni.toggle(121, 81, 0)?;
+    uni.toggle(121, 82, 0)?;
+    uni.toggle(122, 79, 0)?;
+    uni.toggle(122, 82, 0)?;
+    uni.toggle(123, 79, 0)?;
+    uni.toggle(123, 82, 0)?;
+    uni.toggle(125, 79, 0)?;
+    uni.toggle(126, 79, 0)?;
+    uni.toggle(126, 83, 0)?;
+    uni.toggle(127, 80, 0)?;
+    uni.toggle(127, 82, 0)?;
+    uni.toggle(128, 81, 0)?;
+
+    uni.toggle(131, 81, 0)?;
+    uni.toggle(131, 82, 0)?;
+    uni.toggle(132, 81, 0)?;
+    uni.toggle(132, 82, 0)?;
+
+    //Wall in player 0 area!
+    let bw = 5; // buffer width
+
+    // right side
+    for row in (70 - bw)..(83 + bw + 1) {
+        uni.set_unchecked(132 + bw, row, CellState::Wall);
+    }
+
+    // top side
+    for col in (100 - bw)..109 {
+        uni.set_unchecked(col, 70 - bw, CellState::Wall);
+    }
+    for col in 114..(132 + bw + 1) {
+        uni.set_unchecked(col, 70 - bw, CellState::Wall);
+    }
+
+    // left side
+    for row in (70 - bw)..(83 + bw + 1) {
+        uni.set_unchecked(100 - bw, row, CellState::Wall);
+    }
+
+    // bottom side
+    for col in (100 - bw)..120 {
+        uni.set_unchecked(col, 83 + bw, CellState::Wall);
+    }
+    for col in 125..(132 + bw + 1) {
+        uni.set_unchecked(col, 83 + bw, CellState::Wall);
+    }
+
+    //Wall in player 1!
+    for row in 10..19 {
+        uni.set_unchecked(25, row, CellState::Wall);
+    }
+    for col in 10..25 {
+        uni.set_unchecked(col, 10, CellState::Wall);
+    }
+    for row in 11..23 {
+        uni.set_unchecked(10, row, CellState::Wall);
+    }
+    for col in 11..26 {
+        uni.set_unchecked(col, 22, CellState::Wall);
+    }
+
+    Ok(())
+}
+
+fn update_handler(obj: &mut dyn EmitEvent, _uictx: &mut UIContext, evt: &Event) -> Result<Handled, Box<dyn Error>> {
+    let game_area = obj.downcast_mut::<GameArea>().unwrap(); // unwrap OK
+    let game_state = &game_area.game_state;
+
+    if game_state.first_gen_was_drawn && (game_state.running || game_state.single_step) {
+        game_area.uni.next(); // next generation
+        game_state.single_step = false;
+    }
+
+    Ok(NotHandled)
+}
+
+fn keypress_handler(obj: &mut dyn EmitEvent, uictx: &mut UIContext, evt: &Event) -> Result<Handled, Box<dyn Error>> {
+    let game_area = obj.downcast_mut::<GameArea>().unwrap(); // unwrap OK
+    let game_area_state = &mut game_area.game_state;
+
+    if let Some(KeyCodeOrChar::KeyCode(keycode)) = evt.key {
+        match keycode {
+            KeyCode::Key1 => {
+                // pressing 1 clears selection
+                game_area_state.insert_mode = None;
+            }
+            k if k >= KeyCode::Key2 && k <= KeyCode::Key0 => {
+                // select a pattern
+                let grid_info_result = bit_pattern_from_char(&mut uictx.config, keycode);
+                let grid_info = handle_error! {grid_info_result -> (BitGrid, usize, usize),
+                    ConwayError => |e| {
+                        return Err(format!("Invalid pattern bound to keycode {:?}: {}", keycode, e).into())
+                    }
+                }?;
+                game_area_state.insert_mode = Some(grid_info);
+            }
+            KeyCode::Return => {
+                let chatbox_pane_id = self.ui_layout.chatbox_pane_id.clone();
+                uictx.child_event(Event::request_focus(chatbox_pane_id));
+                /*
+                match Pane::widget_from_screen_and_id(&mut self.ui_layout, Screen::Run, &chatbox_pane_id) {
+                    Ok(_chatbox_pane) => {
+                        if let Some(layer) = self.ui_layout.get_screen_layering(Screen::Run) {
+                            layer.enter_focus(
+                                ctx,
+                                &mut self.config,
+                                &mut self.screen_stack,
+                                game_area_state.running,
+                                &chatbox_pane_id,
+                            )?;
+                        }
+                    }
+                    Err(e) => {
+                        error!("Could not get Chatbox's textfield while processing key inputs: {:?}", e);
+                    }
+                }
+                */
+            }
+            KeyCode::R => {
+                if !self.inputs.key_info.repeating {
+                    game_area_state.running = !game_area_state.running;
+                }
+            }
+            KeyCode::Space => {
+                game_area_state.single_step = true;
+            }
+            KeyCode::Up => {
+                game_area_state.arrow_input = (0, -1);
+            }
+            KeyCode::Down => {
+                game_area_state.arrow_input = (0, 1);
+            }
+            KeyCode::Left => {
+                game_area_state.arrow_input = (-1, 0);
+            }
+            KeyCode::Right => {
+                game_area_state.arrow_input = (1, 0);
+            }
+            KeyCode::Add | KeyCode::Equals => {
+                self.viewport.adjust_zoom_level(viewport::ZoomDirection::ZoomIn);
+                let cell_size = self.viewport.get_cell_size();
+                uictx.config.modify(|settings| {
+                    settings.gameplay.zoom = cell_size;
+                });
+            }
+            KeyCode::Minus | KeyCode::Subtract => {
+                self.viewport.adjust_zoom_level(viewport::ZoomDirection::ZoomOut);
+                let cell_size = self.viewport.get_cell_size();
+                uictx.config.modify(|settings| {
+                    settings.gameplay.zoom = cell_size;
+                });
+            }
+            KeyCode::D => {
+                // TODO: do something with this debug code
+                let visibility = None; // can also do Some(player_id)
+                let pat = game_area.uni.to_pattern(visibility);
+                println!("PATTERN DUMP:\n{}", pat.0);
+            }
+            _ => {
+                println!("Unrecognized keycode {:?}", keycode);
+            }
+        }
+    }
+
+    Ok(Handled)
+}
+
+fn mouse_handler(obj: &mut dyn EmitEvent, _uictx: &mut UIContext, evt: &Event) -> Result<Handled, Box<dyn Error>> {
+    let game_area = obj.downcast_mut::<GameArea>().unwrap(); // unwrap OK
+    let game_area_state = &mut game_area.game_state;
+    use ggez::input::mouse::MouseButton;
+
+    if let Some(MouseButton::Left) = evt.button {
+        let mouse_pos = evt.point;
+
+        if let Some((ref grid, width, height)) = game_area.game_state.insert_mode {
+            // inserting a pattern
+            if evt.what == EventType::Click {
+                if let Some(cell) = self.viewport.get_cell(mouse_pos) {
+                    let insert_col = cell.col as isize - (width / 2) as isize;
+                    let insert_row = cell.row as isize - (height / 2) as isize;
+                    let dst_region = Region::new(insert_col, insert_row, width, height);
+                    game_area
+                        .uni
+                        .copy_from_bit_grid(grid, dst_region, Some(CURRENT_PLAYER_ID));
+                }
+            }
+        } else {
+            // not inserting a pattern, just drawing single cells
+            match evt.what {
+                EventType::Click => {
+                    // release
+                    game_area_state.drag_draw = None;
+                }
+                EventType::Drag => {
+                    // hold + motion
+                    if let Some(cell) = self.viewport.get_cell(mouse_pos) {
+                        // Only make dead cells alive
+                        if let Some(cell_state) = game_area_state.drag_draw {
+                            game_area.uni.set(cell.col, cell.row, cell_state, CURRENT_PLAYER_ID);
+                        }
+                    }
+                }
+                EventType::MousePressAndHeld => {
+                    // depress, no move yet
+                    if let Some(cell) = self.viewport.get_cell(mouse_pos) {
+                        if game_area_state.drag_draw.is_none() {
+                            game_area_state.drag_draw =
+                                game_area.uni.toggle(cell.col, cell.row, CURRENT_PLAYER_ID).ok();
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    } else if evt.shift_pressed && game_area_state.arrow_input != (0, 0) {
+        if let Some((ref mut grid, ref mut width, ref mut height)) = game_area_state.insert_mode {
+            let rotation = match game_area_state.arrow_input {
+                (-1, 0) => Some(Rotation::CCW),
+                (1, 0) => Some(Rotation::CW),
+                (0, 0) => unreachable!(),
+                _ => None, // do nothing in this case
+            };
+            if let Some(rotation) = rotation {
+                grid.rotate(*width, *height, rotation).unwrap_or_else(|e| {
+                    error!("Failed to rotate pattern {:?}: {:?}", rotation, e);
+                });
+                // reverse the stored width and height
+                let (new_width, new_height) = (*height, *width);
+                *width = new_width;
+                *height = new_height;
+            } else {
+                info!("Ignoring Shift-<Up/Down>");
+            }
+        }
+    }
+
+    Ok(NotHandled)
+}
+
+/// This takes a keyboard code and returns a `Result` whose Ok value is a `(BitGrid, width,
+/// height)` tuple.
+///
+/// # Errors
+///
+/// This will return an error if the selected RLE pattern is invalid.
+fn bit_pattern_from_char(config: &mut Config, keycode: KeyCode) -> Result<(BitGrid, usize, usize), Box<dyn Error>> {
+    let gameplay = &config.get().gameplay;
+    let rle_str = match keycode {
+        KeyCode::Key2 => &gameplay.pattern2,
+        KeyCode::Key3 => &gameplay.pattern3,
+        KeyCode::Key4 => &gameplay.pattern4,
+        KeyCode::Key5 => &gameplay.pattern5,
+        KeyCode::Key6 => &gameplay.pattern6,
+        KeyCode::Key7 => &gameplay.pattern7,
+        KeyCode::Key8 => &gameplay.pattern8,
+        KeyCode::Key9 => &gameplay.pattern9,
+        KeyCode::Key0 => &gameplay.pattern0,
+        _ => "", // unexpected
+    };
+    let pat = Pattern(rle_str.to_owned());
+    let (width, height) = pat.calc_size()?; // calc_size will fail on invalid RLE -- return it
+    let grid = pat.to_new_bit_grid(width, height)?;
+    Ok((grid, width, height))
 }
 
 impl Widget for GameArea {
@@ -157,3 +508,55 @@ impl Widget for GameArea {
 
 impl_emit_event!(GameArea, self.handler_data);
 widget_from_id!(GameArea);
+
+impl GameArea {
+    pub fn get_game_area_state(self) -> GameAreaState {
+        GameAreaState {
+            first_gen_was_drawn: self.game_state.first_gen_was_drawn,
+            running:             self.game_state.running,
+            single_step:         self.game_state.single_step,
+            arrow_input:         self.game_state.arrow_input,
+            drag_draw:           self.game_state.drag_draw,
+            insert_mode:         self.game_state.insert_mode,
+        }
+    }
+
+    pub fn set_arrow_input(&mut self, input: (isize, isize)) {
+        self.game_state.arrow_input = input;
+    }
+
+    pub fn set_drag_draw(&mut self, dd: Option<CellState>) {
+        self.game_state.drag_draw = dd;
+    }
+
+    pub fn first_gen_drawn(&mut self) {
+        self.game_state.first_gen_was_drawn = true;
+    }
+
+    pub fn insert_mode(&mut self) -> Option<(BitGrid, usize, usize)> {
+        self.game_state.insert_mode
+    }
+}
+
+pub struct GameAreaState {
+    pub first_gen_was_drawn: bool, // The purpose of this is to inhibit gen calc until the first draw
+    pub running:             bool,
+    // Input state
+    pub single_step:         bool,
+    pub arrow_input:         (isize, isize),
+    pub drag_draw:           Option<CellState>,
+    pub insert_mode:         Option<(BitGrid, usize, usize)>, // pattern to be drawn on click along with width and height;
+}
+
+impl Default for GameAreaState {
+    fn default() -> Self {
+        GameAreaState {
+            first_gen_was_drawn: false,
+            running:             false,
+            single_step:         false,
+            arrow_input:         (0, 0),
+            drag_draw:           None,
+            insert_mode:         None,
+        }
+    }
+}

conwayste/src/constants.rs
@@ -19,6 +19,12 @@
 use ggez::graphics::{self, Rect, Scale};
 use std::time::Duration;
 
+// Universe settings
+pub const universe_width_in_cells: usize = 256;
+pub const universe_height_in_cells: usize = 120;
+pub const intro_universe_width_in_cells: usize = 256;
+pub const intro_universe_height_in_cells: usize = 256;
+
 // game play
 pub const CURRENT_PLAYER_ID: usize = 1; // TODO:  get the player ID from server rather than hardcoding
 pub const FOG_RADIUS: usize = 4; // cells

conwayste/src/input.rs
@@ -16,6 +16,7 @@
  *  along with conwayste.  If not, see
  *  <http://www.gnu.org/licenses/>. */
 
+use conway::universe::CellState;
 use ggez::event::{KeyCode, KeyMods, MouseButton};
 use ggez::nalgebra::Point2;
 use std::time::Instant;
@@ -104,6 +105,7 @@ pub struct InputManager {
     pub mouse_info: MouseInfo,
     pub key_info:   KeyInfo,
     pub text_input: Vec<char>,
+    pub drag_draw:  Option<CellState>,
 }
 
 impl InputManager {
@@ -112,6 +114,7 @@ impl InputManager {
             mouse_info: MouseInfo::new(),
             key_info:   KeyInfo::new(),
             text_input: vec![],
+            drag_draw:  None,
         }
     }
 }

conwayste/src/ui/context.rs
@@ -184,9 +184,11 @@ impl<'a> Drop for UIContext<'a> {
 pub enum EventType {
     None,
     Click,
+    DoubleClick,
     KeyPress,
     MouseMove,
     Drag,
+    MousePressAndHeld,
     Translate,
     Resize,
     ParentTranslate,
@@ -379,6 +381,16 @@ impl Event {
     pub fn new_update() -> Self {
         Event {
             what: EventType::Update,
+            ..Default::default()
+        }
+    }
+
+    pub fn new_drag(mouse_point: Point2<f32>, mouse_button: MouseButton, is_shift: bool) -> Self {
+        Event {
+            what: EventType::Drag,
+            point: Some(mouse_point),
+            button: Some(mouse_button),
+            shift_pressed: is_shift,
             ..Default::default()
         }
     }

conwayste/src/ui/layer.rs
@@ -33,7 +33,7 @@ use super::{
     focus::{CycleType, FocusCycle},
     treeview,
     widget::Widget,
-    BoxedWidget, Pane, UIError, UIResult,
+    BoxedWidget, GameAreaState, Pane, UIError, UIResult,
 };
 
 use crate::config;
@@ -373,7 +373,7 @@ impl Layering {
         ggez_context: &mut ggez::Context,
         cfg: &mut config::Config,
         screen_stack: &mut Vec<Screen>,
-        game_in_progress: bool,
+        game_area_state: GameAreaState,
         id: &NodeId,
     ) -> UIResult<()> {
         let focus_cycle = &mut self.focus_cycles[self.highest_z_order];
@@ -397,7 +397,13 @@ impl Layering {
 
         let widget_view = treeview::TreeView::new(&mut self.widget_tree);
 
-        let mut uictx = context::UIContext::new(ggez_context, cfg, widget_view, screen_stack, game_in_progress);
+        let mut uictx = context::UIContext::new(
+            ggez_context,
+            cfg,
+            widget_view,
+            screen_stack,
+            game_area_state.first_gen_was_drawn,
+        );
 
         let mut needs_gain_focus = true;
         if let Some(old_focused_widget) = old_focused_widget {
@@ -476,10 +482,10 @@ impl Layering {
         ggez_context: &mut ggez::Context,
         cfg: &mut config::Config,
         screen_stack: &mut Vec<Screen>,
-        game_in_progress: bool,
+        game_state: &mut GameAreaState,
     ) -> Result<(), Box<dyn Error>> {
         let widget_view = treeview::TreeView::new(&mut self.widget_tree);
-        let mut uictx = context::UIContext::new(ggez_context, cfg, widget_view, screen_stack, game_in_progress);
+        let mut uictx = context::UIContext::new(ggez_context, cfg, widget_view, screen_stack, game_state.running);
         if event.what == context::EventType::Update || event.what == context::EventType::MouseMove {
             Layering::broadcast_event(event, &mut uictx)
         } else if event.is_mouse_event() {
@@ -560,7 +566,6 @@ impl Layering {
         } else {
             // regular key press logic (no focus changes)
             let focused_id = focus_cycle.focused_widget_id();
-            println!("{:?}", focused_id);
             if let Some(id) = focused_id {
                 let id = id.clone();
                 let pane_events = Layering::emit_keyboard_event(event, uictx, &id)?;
@@ -614,7 +619,6 @@ impl Layering {
         let (widget_ref, mut subuictx) = uictx.derive(&focused_id).unwrap(); // unwrap OK b/c NodeId valid & in view
         if let Some(emittable) = widget_ref.as_emit_event() {
             emittable.emit(event, &mut subuictx).map(|handled| {
-                println!("Handled? : {:?}", handled);
                 // Nothing picked up the key event, send it to the Layer itself
                 unhandled_event = handled == context::Handled::NotHandled;
                 let events = subuictx.collect_child_events();

conwayste/src/ui/mod.rs
@@ -42,7 +42,7 @@ pub use chatbox::{Chatbox, ChatboxPublishHandle};
 pub use checkbox::Checkbox;
 pub use common::{center, color_with_alpha, draw_text, intersection, point_offset, within_widget};
 pub use context::{EmitEvent, Event, EventType, UIContext};
-pub use gamearea::GameArea;
+pub use gamearea::{GameArea, GameAreaState};
 pub use label::Label;
 pub use layer::{InsertLocation, Layering};
 pub use pane::Pane;

conwayste/src/ui/textfield.rs
@@ -109,7 +109,7 @@ impl TextField {
         // Handler for accepting keyboard input
         tf.on(EventType::KeyPress, Box::new(TextField::key_handler)).unwrap();
 
-        // Set handlers for toggling has_keyboard_focus
+        // Set handlers for toggling focus
         tf.on(EventType::GainFocus, Box::new(TextField::gain_focus_handler))
             .unwrap();
         tf.on(EventType::LoseFocus, Box::new(TextField::lose_focus_handler))

commit a7da51bbccbeb6b521604525a9512c33f179d180

Fixed regression bug on patatern rotation
Broken in fe762e635c813679e5fc3d75c75b97f50fa3e286

conwayste/src/ui/gamearea.rs
@@ -277,6 +277,28 @@ impl GameArea {
             }
         }
 
+        if evt.shift_pressed && game_area_state.arrow_input != (0, 0) {
+            if let Some((ref mut grid, ref mut width, ref mut height)) = game_area_state.insert_mode {
+                let rotation = match game_area_state.arrow_input {
+                    (-1, 0) => Some(Rotation::CCW),
+                    (1, 0) => Some(Rotation::CW),
+                    (0, 0) => unreachable!(),
+                    _ => None, // do nothing in this case
+                };
+                if let Some(rotation) = rotation {
+                    grid.rotate(*width, *height, rotation).unwrap_or_else(|e| {
+                        error!("Failed to rotate pattern {:?}: {:?}", rotation, e);
+                    });
+                    // reverse the stored width and height
+                    let (new_width, new_height) = (*height, *width);
+                    *width = new_width;
+                    *height = new_height;
+                } else {
+                    info!("Ignoring Shift-<Up/Down>");
+                }
+            }
+        }
+
         Ok(Handled)
     }
 
@@ -338,27 +360,6 @@ impl GameArea {
                         }
                     }
                     _ => {}
-                }
-            }
-        } else if evt.shift_pressed && game_area_state.arrow_input != (0, 0) {
-            if let Some((ref mut grid, ref mut width, ref mut height)) = game_area_state.insert_mode {
-                let rotation = match game_area_state.arrow_input {
-                    (-1, 0) => Some(Rotation::CCW),
-                    (1, 0) => Some(Rotation::CW),
-                    (0, 0) => unreachable!(),
-                    _ => None, // do nothing in this case
-                };
-                if let Some(rotation) = rotation {
-                    grid.rotate(*width, *height, rotation).unwrap_or_else(|e| {
-                        error!("Failed to rotate pattern {:?}: {:?}", rotation, e);
-                    });
-                    // reverse the stored width and height
-                    let (new_width, new_height) = (*height, *width);
-                    *width = new_width;
-                    *height = new_height;
-                    event_handled = Handled;
-                } else {
-                    info!("Ignoring Shift-<Up/Down>");
                 }
             }
         }