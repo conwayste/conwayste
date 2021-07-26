25f1d42bf2c59ff6b42d085c687cb9b7bdf7df0f
manghi <manghirs@gmail.com>
2021-07-25 17:49:44 -0700

    [Failing] Added unit tests for sorted buffer
    The unit tests that fail are due to an arbitrary iteration order
    performed by the `Reverse<BinaryHeap>`. It only guarantees `.pop()`
    returns the smallest value.

--- netwaystev2/src/filter/sortedbuffer.rs
+++ netwaystev2/src/filter/sortedbuffer.rs
@@ -54,6 +54,13 @@ impl<T> SequencedMinHeap<T> {
     pub fn take(&mut self) -> Option<T> {
         self.heap.pop().map(|reversed_tup| reversed_tup.0.1)
     }
+
+    #[cfg(test)]
+    pub fn print(&self) {
+        for tuple in &self.heap {
+            println!("Key: {}", tuple.0.0);
+        }
+    }
 }
 
 struct SequencedItem<T>(u64, T);
@@ -78,4 +85,167 @@ impl<T> PartialOrd for SequencedItem<T> {
 
 // Marker trait impl for Ord without needing to pass `Eq` down to the item
 impl<T> Eq for SequencedItem<T> {
+}
+
+#[cfg(test)]
+mod test {
+    use super::SequencedMinHeap;
+
+    #[test]
+    fn seqminheap_empty_checks() {
+        let smh = SequencedMinHeap::<usize>::new();
+
+        assert_eq!(smh.contains_sequence_number(0), false);
+        assert_eq!(smh.count_contiguous(0), 0);
+        assert_eq!(smh.peek_sequence_number(), None);
+    }
+
+    #[test]
+    fn seqminheap_filled_checks() {
+        let mut smh = SequencedMinHeap::<usize>::new();
+
+        for i in 2..4 {
+            smh.add(i, 0);
+        }
+
+        assert_eq!(smh.contains_sequence_number(0), false);
+        assert_eq!(smh.count_contiguous(0), 0);
+        assert_eq!(smh.peek_sequence_number(), Some(2));
+    }
+
+/*
+ * The next few tests fail because the underlying BinaryHeap makes no guarantee about iteration order. It's arbitrary.
+ */
+
+    #[test]
+    fn seqminheap_insert_ascending() {
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for i in 0..10 {
+            smh.add(i, 0);
+        }
+
+        for i in 0..10 {
+            assert_eq!(smh.contains_sequence_number(i), true);
+        }
+
+        for i in 0..10 {
+            assert_eq!(smh.count_contiguous(0), 10 - i);
+        }
+    }
+
+    #[test]
+    fn seqminheap_insert_descending() {
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for i in 10..0 {
+            smh.add(i, 0);
+        }
+
+        for i in 0..10 {
+            assert_eq!(smh.contains_sequence_number(i), true);
+        }
+
+        for i in 0..10 {
+            assert_eq!(smh.count_contiguous(0), 10 - i);
+        }
+    }
+
+    #[test]
+    fn seqminheap_insert_sequential_with_gaps() {
+        use rand::distributions::{Distribution, Uniform};
+        let mut rng = rand::thread_rng();
+
+        let span = [1, 2, 5, 6, 9];
+
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for n in &span {
+            smh.add(*n, 0);
+        }
+
+        for x in &span {
+            assert_eq!(smh.contains_sequence_number(*x), true);
+        }
+
+        assert_eq!(smh.count_contiguous(0), 0);
+        assert_eq!(smh.count_contiguous(1), 2);
+        assert_eq!(smh.count_contiguous(2), 1);
+        assert_eq!(smh.count_contiguous(5), 2);
+        assert_eq!(smh.count_contiguous(6), 1);
+        assert_eq!(smh.count_contiguous(9), 1);
+        assert_eq!(smh.count_contiguous(10), 0);
+    }
+
+    #[test]
+    fn seqminheap_insert_reverse_sequential_with_gaps() {
+        use rand::distributions::{Distribution, Uniform};
+        let mut rng = rand::thread_rng();
+
+        let mut span : Vec<u64> = vec![1, 2, 5, 6, 9];
+        span.reverse();
+
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for n in &span {
+            smh.add(*n, 0);
+        }
+
+        for x in &span {
+            assert_eq!(smh.contains_sequence_number(*x), true);
+        }
+
+        assert_eq!(smh.count_contiguous(0), 0);
+        assert_eq!(smh.count_contiguous(1), 2);
+        assert_eq!(smh.count_contiguous(2), 1);
+        assert_eq!(smh.count_contiguous(5), 2);
+        assert_eq!(smh.count_contiguous(6), 1);
+        assert_eq!(smh.count_contiguous(9), 1);
+        assert_eq!(smh.count_contiguous(10), 0);
+    }
+
+    #[test]
+    fn seqminheap_insert_out_of_order_with_gaps() {
+        use rand::distributions::{Distribution, Uniform};
+        let mut rng = rand::thread_rng();
+
+        let mut span : Vec<u64> = vec![1, 2, 5, 6, 9];
+        span.reverse();
+
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for n in &span {
+            smh.add(*n, 0);
+        }
+
+        for x in &span {
+            assert_eq!(smh.contains_sequence_number(*x), true);
+        }
+
+        smh.print();
+
+        assert_eq!(smh.count_contiguous(0), 0);
+        assert_eq!(smh.count_contiguous(1), 2);
+        assert_eq!(smh.count_contiguous(2), 1);
+        assert_eq!(smh.count_contiguous(5), 2);
+        assert_eq!(smh.count_contiguous(6), 1);
+        assert_eq!(smh.count_contiguous(9), 1);
+        assert_eq!(smh.count_contiguous(10), 0);
+    }
+
+    fn seqminheap_insert_out_of_order_with_no_gaps() {
+        use rand::distributions::{Distribution, Uniform};
+        let mut rng = rand::thread_rng();
+
+        let mut span : Vec<u64> = vec![1, 2, 5, 6, 9];
+        span.reverse();
+
+        let mut smh = SequencedMinHeap::<usize>::new();
+        for n in &span {
+            smh.add(*n, 0);
+        }
+
+        for x in &span {
+            assert_eq!(smh.contains_sequence_number(*x), true);
+        }
+
+        for i in 0..10 {
+            assert_eq!(smh.count_contiguous(i), 10usize - i as usize);
+        }
+    }
 }
