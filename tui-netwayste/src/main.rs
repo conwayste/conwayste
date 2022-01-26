7faa8d5044385abc9c07ac14775255923078754f
Aaron Miller <aaron@iforgotmy.name>
2022-01-25 21:02:21 -0800

    TUI: replaced (String,String) with Field and removed some mem::replace

--- tui-netwayste/src/main.rs
+++ tui-netwayste/src/main.rs
@@ -54,11 +54,11 @@ impl InputStage {
 }
 
 struct EditableCommand {
-    fields: StatefulList<(String, String)>,
+    fields: StatefulList<Field>,
 }
 
 impl EditableCommand {
-    fn with_items(list_fields: Vec<(String, String)>) -> Self {
+    fn with_items(list_fields: Vec<Field>) -> Self {
         EditableCommand {
             fields: StatefulList::with_items(list_fields),
         }
@@ -85,8 +85,23 @@ struct App<'a> {
     menu_display_index: usize,
     menus:              Vec<StatefulList<String>>,
     menu_item_map:      HashMap<String, MenuItemEntry>,
-    displayed_editor:   StatefulList<(String, String)>,
+    displayed_editor:   StatefulList<Field>,
     events:             Vec<(&'a str, &'a str)>,
+}
+
+#[derive(Debug, Clone)]
+struct Field {
+    name: String,
+    value: String,
+}
+
+impl Field {
+    fn new(name: &str, value: &str) -> Self {
+        Field {
+            name: name.into(),
+            value: value.into(),
+        }
+    }
 }
 
 impl<'a> App<'a> {
@@ -99,9 +114,9 @@ impl<'a> App<'a> {
             menu_item_map.insert(
                 ra.to_string().clone(),
                 MenuItemEntry::EditDialog(EditableCommand::with_items(vec![
-                    ("Field 1".to_owned(), "DefaultValue".to_owned()),
-                    ("Field 2".to_owned(), "124".to_owned()),
-                    ("Field 3".to_owned(), "".to_owned()),
+                    Field::new("Field 1", "DefaultValue"),
+                    Field::new("Field 2", "124"),
+                    Field::new("Field 3", ""),
                 ])),
             );
         }
@@ -110,9 +125,9 @@ impl<'a> App<'a> {
             menu_item_map.insert(
                 rc.to_string().clone(),
                 MenuItemEntry::EditDialog(EditableCommand::with_items(vec![
-                    ("Field 1".to_owned(), "DefaultValue".to_owned()),
-                    ("Field 2".to_owned(), "124".to_owned()),
-                    ("Field 3".to_owned(), "".to_owned()),
+                    Field::new("Field 1", "DefaultValue"),
+                    Field::new("Field 2", "124"),
+                    Field::new("Field 3", ""),
                 ])),
             );
         }
@@ -270,14 +285,14 @@ fn handle_command_keys(key: KeyCode, app: &mut App) {
             if let Some(next_menu_index) = opt_next_menu_idx {
                 // Save the state current menu and load the new menu
                 let current_menu = std::mem::replace(&mut app.displayed_menu, app.menus[*next_menu_index].clone());
-                let _ = std::mem::replace(&mut app.menus[app.menu_display_index], current_menu);
+                app.menus[app.menu_display_index] = current_menu;
                 app.menu_display_index = *next_menu_index;
             }
             if let Some(edit_cmd_ref) = opt_edit_cmd_ref {
                 // Save the menu state; Load the edit state
                 //let _ = std::mem::replace(&mut app.menus[app.menu_display_index], app.displayed_menu);
 
-                let _ = std::mem::replace(&mut app.displayed_editor, edit_cmd_ref.fields.clone());
+                app.displayed_editor = edit_cmd_ref.fields.clone();
 
                 app.input_stage.next();
             }
@@ -289,11 +304,11 @@ fn handle_command_keys(key: KeyCode, app: &mut App) {
 
             if app.input_stage == InputStage::NavigateMenu {
                 // Save current menu state and load the parent menu
-                let _ = std::mem::replace(&mut app.menus[app.menu_display_index], app.displayed_menu.clone());
+                app.menus[app.menu_display_index] = app.displayed_menu.clone();
 
                 // HACK: We only have two levels of menus now. It isn't worth the time to build a proper menu tree
                 app.menu_display_index = 0;
-                let _ = std::mem::replace(&mut app.displayed_menu, app.menus[app.menu_display_index].clone());
+                app.displayed_menu = app.menus[app.menu_display_index].clone();
             } else {
                 app.input_stage.prev();
             }
@@ -311,7 +326,7 @@ fn handle_navigateedit_keys(key: KeyCode, app: &mut App) {
             if !app.editing {
                 app.editing = true;
                 let index = app.displayed_editor.state.selected().unwrap();
-                app.preedit_text = app.displayed_editor.items[index].1.clone();
+                app.preedit_text = app.displayed_editor.items[index].value.clone();
             }
         }
         KeyCode::Esc => {
@@ -330,7 +345,7 @@ fn handle_navigateedit_keys(key: KeyCode, app: &mut App) {
             let edit_cmd_mut_ref = opt_edit_cmd.unwrap();
 
             // Save the editing window state
-            let _ = std::mem::replace(&mut edit_cmd_mut_ref.fields, app.displayed_editor.clone());
+            edit_cmd_mut_ref.fields = app.displayed_editor.clone();
             app.input_stage.prev();
         }
         _ => (),
@@ -338,7 +353,7 @@ fn handle_navigateedit_keys(key: KeyCode, app: &mut App) {
 }
 
 fn handle_editcommand_keys(key: KeyCode, app: &mut App) {
-    let field_value = &mut app.displayed_editor.items[app.displayed_editor.state.selected().unwrap()].1;
+    let field_value = &mut app.displayed_editor.items[app.displayed_editor.state.selected().unwrap()].value;
 
     match key {
         KeyCode::Char(ch) => field_value.push(ch),
@@ -351,7 +366,7 @@ fn handle_editcommand_keys(key: KeyCode, app: &mut App) {
             app.editing = false;
 
             let index = app.displayed_editor.state.selected().unwrap();
-            app.displayed_editor.items[index].1 = std::mem::take(&mut app.preedit_text);
+            app.displayed_editor.items[index].value = std::mem::take(&mut app.preedit_text);
         }
         _ => (),
     }
@@ -466,7 +481,7 @@ fn editor_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         ]
     };
 
-    let mut title = Spans::from(msg);
+    let title = Spans::from(msg);
 
     let block = Block::default()
         .borders(Borders::ALL)
@@ -482,8 +497,8 @@ fn editor_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .displayed_editor
         .items
         .iter()
-        .map(|(field_name, field_value)| {
-            let lines = vec![Spans::from(format!("{} -> {}", field_name, field_value))];
+        .map(|field| {
+            let lines = vec![Spans::from(format!("{} -> {}", field.name, field.value))];
             ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
         })
         .collect();
@@ -497,8 +512,8 @@ fn editor_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
 
     if app.editing {
         let index = app.displayed_editor.state.selected().unwrap();
-        let label_text = &app.displayed_editor.items[index].0;
-        let editing_text = &app.displayed_editor.items[index].1;
+        let label_text = &app.displayed_editor.items[index].name;
+        let editing_text = &app.displayed_editor.items[index].value;
         f.set_cursor(
             area.x
                 + ">> ".width() as u16
