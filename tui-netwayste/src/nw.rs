be997445fbe5ef8fbb08ed14f83a188670a85e02
mang <manghirs@gmail.com>
2022-07-27 20:43:42 -0700

    Request action list generated

--- tui-netwayste/src/nw.rs
+++ tui-netwayste/src/nw.rs
@@ -3,6 +3,7 @@ use crate::nw_protocol::*;
 use netwaystev2::{
     filter::FilterMode,
 };
+use strum::IntoEnumIterator;
 
 pub fn create_packet_selection_lists(mode: FilterMode) -> Vec<StatefulList<String>> {
     match mode {
@@ -13,8 +14,7 @@ pub fn create_packet_selection_lists(mode: FilterMode) -> Vec<StatefulList<Strin
                 inner: RequestActionConnect::try_from(vec!["hi".to_owned(), "hello".to_owned()]).expect("fail"),
             };
 
-            let ra_list = StatefulList::with_items(vec!["RA_one".to_owned(), "RA_two".to_owned()]);
-
+            let ra_list = StatefulList::with_items(RequestAction::iter().map(|ra| ra.to_string()).collect());
             let rc_list = StatefulList::with_items(vec!["RC_one".to_owned(), "RC_two".to_owned()]);
 
             vec![client_packets, ra_list, rc_list]
