40b2c9e9711764ac43eac6f0d057b05145b4f46d
Aaron Miller <aaron@iforgotmy.name>
2022-08-11 22:29:25 -0700

    add common::Endpoint wrapper, in preparation for wrapping transport

---
+++ nwv2-python-wrapper/src/common.rs
@@ -0,0 +1,32 @@
+use std::net::SocketAddr;
+
+use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
+use netwaystev2::common::Endpoint;
+
+#[pyclass]
+pub struct EndpointW {
+    inner: Endpoint,
+}
+
+impl Into<Endpoint> for EndpointW {
+    fn into(self) -> Endpoint {
+        self.inner
+    }
+}
+
+#[pymethods]
+impl EndpointW {
+    #[new]
+    fn new(host_and_port: String) -> PyResult<Self> {
+        let sa: SocketAddr = host_and_port.parse().map_err(|e| {
+            return PyValueError::new_err(format!("failed to parse SocketAddr string for Endpoint: {}", e));
+        })?;
+        Ok(EndpointW { inner: Endpoint(sa) })
+    }
+
+    fn __repr__(&self) -> String {
+        format!("{:?}", self.inner)
+    }
+}
--- nwv2-python-wrapper/src/lib.rs
+++ nwv2-python-wrapper/src/lib.rs
@@ -1,9 +1,11 @@
 ///! Reference: https://pyo3.rs/v0.16.4/ecosystem/async-await.html#pyo3-native-rust-modules
 
-pub mod protocol;
+mod protocol;
+pub(crate) mod common;
 pub(crate) mod utils;
 use protocol::request::RequestActionW;
 use protocol::packet::PacketW;
+use common::EndpointW;
 
 use pyo3_asyncio;
 use pyo3::prelude::*;
@@ -13,7 +15,8 @@ use pyo3::prelude::*;
 #[pymodule]
 fn nwv2_python_wrapper(_py: Python, m: &PyModule) -> PyResult<()> {
     // m.add_function(wrap_pyfunction!(rust_delayed_value, m)?)?;
-    m.add_class::<PacketW>()?;
     m.add_class::<RequestActionW>()?;
+    m.add_class::<PacketW>()?;
+    m.add_class::<EndpointW>()?;
     Ok(())
 }
