d6c807470f281a534c286e6ddd8e47ffdf489ccb
Aaron Miller <aaron@iforgotmy.name>
2022-08-11 22:06:56 -0700

    refactor Python wrappers

--- nwv2-python-wrapper/src/lib.rs
+++ nwv2-python-wrapper/src/lib.rs
@@ -1,68 +1,12 @@
 ///! Reference: https://pyo3.rs/v0.16.4/ecosystem/async-await.html#pyo3-native-rust-modules
 
-use std::collections::HashMap;
-
-pub mod wrappers;
+pub mod protocol;
 pub(crate) mod utils;
-use wrappers::request::*;
-use utils::get_from_dict;
+use protocol::request::RequestActionW;
+use protocol::packet::PacketW;
 
 use pyo3_asyncio;
 use pyo3::prelude::*;
-use pyo3::exceptions::PyValueError;
-
-use netwaystev2::protocol::Packet;
-use netwaystev2::protocol::RequestAction;
-
-/// A wrapped netwaystev2 Packet
-///
-/// Example usage:
-///
-/// ```python
-/// p = PacketW("request", "fakecookie")
-/// ```
-#[pyclass]
-struct PacketW {
-    inner: Packet,
-}
-
-#[pymethods]
-impl PacketW {
-    #[new]
-    #[args(kwds="**")]
-    fn new(variant: String, kwds: Option<HashMap<String,&PyAny>>) -> PyResult<Self> {
-        let kwds = if let Some(kwds) = kwds {
-            kwds
-        } else {
-            HashMap::new()
-        };
-        let packet = match variant.to_lowercase().as_str() {
-            "request" => {
-                let sequence: u64 = get_from_dict(&kwds, "sequence")?;
-                let response_ack: Option<u64> = get_from_dict(&kwds, "response_ack")?;
-                let action_wrapper: RequestActionW = get_from_dict(&kwds, "action")?;
-                let cookie: Option<String> = get_from_dict(&kwds, "cookie")?;
-                Packet::Request {
-                    sequence,
-                    response_ack,
-                    action: action_wrapper.into(),
-                    cookie,
-                }
-            }
-            // TODO: more variants
-            _ => {
-                return Err(PyValueError::new_err(format!("invalid variant type: {}", variant)));
-            }
-        };
-        Ok(PacketW { inner: packet })
-    }
-
-    fn __repr__(&self) -> String {
-        format!("{:?}", self.inner)
-    }
-
-    // TODO: methods for getting/setting stuff in a packet
-}
 
 
 /// A Python module implemented in Rust.
---
+++ nwv2-python-wrapper/src/protocol/packet.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
+use netwaystev2::protocol::Packet;
+use super::request::RequestActionW;
+use crate::utils::get_from_dict;
+
+/// A wrapped netwaystev2 Packet
+///
+/// Example usage:
+///
+/// ```python
+/// p = PacketW("request", "fakecookie")
+/// ```
+#[pyclass]
+pub struct PacketW {
+    inner: Packet,
+}
+
+#[pymethods]
+impl PacketW {
+    #[new]
+    #[args(kwds="**")]
+    fn new(variant: String, kwds: Option<HashMap<String,&PyAny>>) -> PyResult<Self> {
+        let kwds = if let Some(kwds) = kwds {
+            kwds
+        } else {
+            HashMap::new()
+        };
+        let packet = match variant.to_lowercase().as_str() {
+            "request" => {
+                let sequence: u64 = get_from_dict(&kwds, "sequence")?;
+                let response_ack: Option<u64> = get_from_dict(&kwds, "response_ack")?;
+                let action_wrapper: RequestActionW = get_from_dict(&kwds, "action")?;
+                let cookie: Option<String> = get_from_dict(&kwds, "cookie")?;
+                Packet::Request {
+                    sequence,
+                    response_ack,
+                    action: action_wrapper.into(),
+                    cookie,
+                }
+            }
+            // TODO: more variants
+            _ => {
+                return Err(PyValueError::new_err(format!("invalid variant type: {}", variant)));
+            }
+        };
+        Ok(PacketW { inner: packet })
+    }
+
+    fn __repr__(&self) -> String {
+        format!("{:?}", self.inner)
+    }
+
+    // TODO: methods for getting/setting stuff in a packet
+}
