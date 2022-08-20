17c0967dffaad582e815290d21674e5e04d01a2a
Aaron Miller <aaron@iforgotmy.name>
2022-08-20 15:23:03 -0700

    support running the python-wrapped Transport; next is sending and receiving on channels

--- nwv2-python-wrapper/src/transport/mod.rs
+++ nwv2-python-wrapper/src/transport/mod.rs
@@ -1,3 +1,4 @@
+use futures_util::future::TryFutureExt;
 use pyo3::prelude::*;
 use pyo3::exceptions::*;
 
@@ -11,13 +12,14 @@ use netwaystev2::transport::{
 
 #[pyclass]
 pub struct TransportInterface {
-    transport: Transport,
+    transport: Option<Transport>,
     cmd_tx: TransportCmdSend,
     response_rx: TransportRspRecv,
     notify_rx: TransportNotifyRecv,
 }
 
-// This can't be a #[new] constructor because it's Python async.
+/// Create a TransportInterface.
+/// This can't be a #[new] constructor because it's Python async.
 #[pyfunction]
 pub fn new_transport_interface<'p>(py: Python<'p>, opt_host: Option<String>, opt_port: Option<u16>) -> PyResult<&'p PyAny> {
     let err_mapper = |e| {
@@ -26,7 +28,7 @@ pub fn new_transport_interface<'p>(py: Python<'p>, opt_host: Option<String>, opt
     let transport_fut = async move {
         let (transport, cmd_tx, response_rx, notify_rx) = Transport::new(opt_host, opt_port).await.map_err(err_mapper)?;
         Ok(TransportInterface{
-            transport,
+            transport: Some(transport),
             cmd_tx,
             response_rx,
             notify_rx,
@@ -35,11 +37,26 @@ pub fn new_transport_interface<'p>(py: Python<'p>, opt_host: Option<String>, opt
     pyo3_asyncio::tokio::future_into_py(py, transport_fut)
 }
 
-//XXX pyfunction run_transport_interface
-
 #[pymethods]
 impl TransportInterface  {
+    /// Runs the Transport. The Python Future will complete when the Transport exits.
+    fn run<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
+        let mut transport = self.transport.take().ok_or_else(|| PyException::new_err("cannot call run() more than once"))?;
+        let run_fut = async move {
+            // Use .await.map_err(...) to get rid of the anyhow
+            transport.run().await.map_err(|e| PyException::new_err(format!("error from run(): {}", e)))
+        };
+        pyo3_asyncio::tokio::future_into_py(py, run_fut)
+    }
+
+    //XXX allow receiving and sending on channels.
+
     fn __repr__(&self) -> String {
-        "<(Transport, TransportCmdSend, TransportRspRecv, TransportNotifyRecv)>".to_owned()
+        format!(
+            "TransportInterface{{ transport: {},   cmd_tx: {:?},   response_rx: {:?},   notify_rx: {:?} }}",
+            if self.transport.is_some() { "Some(<Transport>)" } else { "None" }, // run() takes this; keep borrow
+                                                                                 // checker happy
+            self.cmd_tx, self.response_rx, self.notify_rx,
+        )
     }
 }
