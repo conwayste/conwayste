b93ee4a5ba28958590c89245075784ed61f82dd1
Aaron Miller <aaron@iforgotmy.name>
2022-12-20 22:33:30 -0800

    Filter bug fix: default retry interval was way too low (retrying way too often)

--- netwaystev2/src/settings.rs
+++ netwaystev2/src/settings.rs
@@ -6,5 +6,5 @@ pub const TRANSPORT_CHANNEL_LEN: usize = 1000;
 pub const FILTER_CHANNEL_LEN: usize = 1000;
 pub const APP_CHANNEL_LEN: usize = 1000;
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-pub const DEFAULT_RETRY_INTERVAL: Duration = Duration::from_millis(5);
+pub const DEFAULT_RETRY_INTERVAL: Duration = Duration::from_millis(250);
 pub const TRANSPORT_RETRY_COUNT_LOG_THRESHOLD: usize = 10;
