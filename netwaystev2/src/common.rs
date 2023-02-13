7707e6f6981de5a29d3d439eabad7f92592da9ec
Aaron Miller <aaron@iforgotmy.name>
2023-02-12 16:44:19 -0800

    add a comment

--- netwaystev2/src/common.rs
+++ netwaystev2/src/common.rs
@@ -5,4 +5,6 @@ use std::pin::Pin;
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct Endpoint(pub SocketAddr);
 
+/// A future that completes when the layer has been shutdown. This is returned by the
+/// get_shutdown_watcher() method of various layers.
 pub type ShutdownWatcher = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
