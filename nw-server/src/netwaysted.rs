800f4e12b3a226133d2ab343657c169ed0a7771e
mang <manghirs@gmail.com>
2023-04-06 21:25:49 -0700

    pretty-print nw-server config

--- nw-server/src/netwaysted.rs
+++ nw-server/src/netwaysted.rs
@@ -1,4 +1,44 @@
+use anyhow;
+use clap::{self, Parser};
+use serde::Deserialize;
+use toml;
 
-fn main() -> Result<(), ()> {
+#[derive(Deserialize, Debug)]
+struct Config {
+    server: ServerConfig,
+    registry: Option<RegistryConfig>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ServerConfig {
+    name: String,
+    bind_host: String,
+    bind_port: u16,
+}
+
+#[derive(Deserialize, Debug)]
+struct RegistryConfig {
+    public_host: String,
+    public_port: u16,
+    url: String,
+}
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    config_file: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let toml_config_str = std::fs::read_to_string(args.config_file)?;
+    let toml_config: Config = toml::from_str(toml_config_str.as_str())?;
+
+    println!("{:#?}", toml_config);
+
     Ok(())
 }
