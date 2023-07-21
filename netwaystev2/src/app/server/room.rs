fa3abe4ca49b337719a73886524dfcc4a3ed6c2b
mang <manghirs@gmail.com>
2023-07-20 21:32:09 -0700

    significantly simplified server room design

--- netwaystev2/src/app/server/room.rs
+++ netwaystev2/src/app/server/room.rs
@@ -1,32 +1,12 @@
-#![allow(unused)] // XXX
-
 use anyhow::{anyhow, Result};
-use indexmap::IndexMap;
 use snowflake::ProcessUniqueId;
 
-const MAX_ROOM_NAME_CHARS: usize = 32;
+use crate::protocol::RoomStatus;
+
 const ROOMS_PER_SERVER: usize = 3; // TODO: Move to config
-const MODULE_NAME: &'static str = "Room";
 
 // XXX PlayerId is defined here just for bring up. Might be best to relocate when ready.
 type PlayerId = ProcessUniqueId;
-type RoomId = ProcessUniqueId;
-
-#[derive(Debug, thiserror::Error)]
-pub enum RoomMgrError {
-    #[error("Failed to create a new room, the server is full")]
-    AllRoomsAllocated,
-    #[error("A room with that name already exists: {name}")]
-    RoomWithNameExists { name: String },
-    #[error("Room does not exist. id={id}")]
-    RoomIdNotFound { id: RoomId },
-    #[error("Room does not exist. name={name}")]
-    RoomNameNotFound { name: String},
-    #[error("Invalid room name provided: {name}")]
-    InvalidRoomName { name: String },
-    #[error("Internal Room Manager error")]
-    RoomMgmtError { context: String },
-}
 
 #[derive(Default)]
 pub struct Room {
@@ -38,177 +18,30 @@ pub struct Room {
 impl Room {
     fn with_name(self, name: &str) -> Room {
         Room {
-            name: name.to_owned(),
+            name:     name.to_owned(),
             player_a: self.player_a,
             player_b: self.player_b,
         }
     }
 }
 
-pub struct RoomBlock {
-    names2rid: IndexMap<String, RoomId>,
-    rid2room:  IndexMap<RoomId, Room>,
+pub struct ServerRooms {
+    rooms: Vec<Room>,
 }
 
-impl RoomBlock {
-    pub fn new() -> RoomBlock {
-        RoomBlock {
-            names2rid: IndexMap::new(),
-            rid2room:  IndexMap::new(),
-        }
+impl ServerRooms {
+    pub fn new() -> ServerRooms {
+        let rooms = (0..ROOMS_PER_SERVER)
+            .map(|i| Room::default().with_name(&format!("Room{}", i)))
+            .collect();
+        ServerRooms { rooms }
     }
 
-    pub fn count(&self) -> Result<usize> {
-        if self.rid2room.len() == self.names2rid.len() {
-            Ok(self.rid2room.len())
-        } else {
-            let error = RoomMgrError::RoomMgmtError {
-                context: "Length mismatch between room IDs and room names".to_owned(),
-            };
-            error!("[{}] {}", MODULE_NAME, error);
-            Err(anyhow!(error))
-        }
-    }
-
-    pub fn alloc(&mut self, room_name: String) -> Result<RoomId> {
-        trace!("[{}] allocating room => Name:{}", MODULE_NAME, room_name);
-
-        if self.count()? >= ROOMS_PER_SERVER {
-            let error = RoomMgrError::AllRoomsAllocated;
-            error!("[{}] {}", MODULE_NAME, error);
-            return Err(anyhow!(error));
-        }
-
-        if room_name == "" || room_name.len() > MAX_ROOM_NAME_CHARS {
-            let error = RoomMgrError::InvalidRoomName { name: room_name };
-            error!("[{}] {}", MODULE_NAME, error);
-            return Err(anyhow!(error));
-        }
-
-        if self.names2rid.contains_key(&room_name) {
-            let error = RoomMgrError::RoomWithNameExists { name: room_name };
-            error!("[{}] {}", MODULE_NAME, error);
-            return Err(anyhow!(error));
-        }
-
-        let room_id = RoomId::new();
-        trace!("[{}] room allocated => ID:{} Name:{} ", MODULE_NAME, room_id, room_name);
-
-        let room = Room::default().with_name(&room_name);
-
-        // Return value ignored is okay the room ID is uniquely generated
-        let _ = self.names2rid.insert(room_name, room_id);
-        // Return value ignored is okay because of contains_key() check
-        let _ = self.rid2room.insert(room_id, room);
-
-        Ok(room_id)
-    }
-
-    pub fn free(&mut self, room_id: RoomId) -> Result<()> {
-        trace!("[{}] freeing room => ID:{}", MODULE_NAME, room_id);
-
-        if let Some(room) = self.rid2room.remove(&room_id) {
-            if let Some(_room_id) = self.names2rid.remove(&room.name) {
-                trace!("[{}] room '{}' freed ", MODULE_NAME, room.name);
-            } else {
-                error!(
-                    "[{}] failed to find room by name => ID:{} Name:{} ",
-                    MODULE_NAME, room_id, room.name
-                );
-            }
-        } else {
-            return Err(anyhow!(RoomMgrError::RoomIdNotFound { id: room_id }));
-        }
-
-        Ok(())
-    }
-
-    pub fn get(&self, room_id: RoomId) -> Result<&Room> {
-        if let Some(room) = self.rid2room.get(&room_id) {
-            return Ok(room);
-        } else {
-            return Err(anyhow!(RoomMgrError::RoomIdNotFound { id: room_id }));
-        }
-    }
-
-    pub fn get_mut(&mut self, room_id: RoomId) -> Result<&mut Room> {
-        if let Some(room) = self.rid2room.get_mut(&room_id) {
-            return Ok(room);
-        } else {
-            return Err(anyhow!(RoomMgrError::RoomIdNotFound { id: room_id }));
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::MAX_ROOM_NAME_CHARS;
-
-    use super::{RoomBlock, RoomId, ROOMS_PER_SERVER};
-
-    #[test]
-    fn test_allocate_all_rooms() {
-        let mut rooms = RoomBlock::new();
-
-        for i in 0..ROOMS_PER_SERVER {
-            assert!(rooms.alloc(format!("room {}", i)).is_ok());
-        }
-    }
-
-    #[test]
-    fn test_allocation_fails_when_full() {
-        let mut rooms = RoomBlock::new();
-
-        for i in 0..ROOMS_PER_SERVER {
-            assert!(rooms.alloc(format!("room {}", i)).is_ok());
-
-            let _ = rooms.count(); // Performs a size assertion internally
-        }
-
-        let allocation_should_fail = rooms.alloc("allocation-should-fail".to_owned());
-        assert!(allocation_should_fail.is_err());
-    }
-
-    #[test]
-    fn test_allocation_fails_duplicate() {
-        let mut rooms = RoomBlock::new();
-
-        assert!(rooms.alloc("room-name".into()).is_ok());
-        assert!(rooms.alloc("room-name".into()).is_err());
-    }
-
-    #[test]
-    fn test_allocation_fails_invalid_name() {
-        let mut rooms = RoomBlock::new();
-
-        let long_name = "0".repeat(MAX_ROOM_NAME_CHARS + 1);
-
-        assert!(rooms.alloc("".into()).is_err());
-        assert!(rooms.alloc(long_name).is_err());
-    }
-
-    #[test]
-    fn test_free_all() {
-        let mut rooms = RoomBlock::new();
-
-        let mut room_id_list = vec![];
-        for i in 0..ROOMS_PER_SERVER {
-            let room_id = rooms.alloc(format!("room {}", i)).expect("room allocation failed");
-            room_id_list.push(room_id);
-            let _ = rooms.count(); // Performs a size assertion internally
-        }
-
-        for r in room_id_list {
-            assert!(rooms.free(r).is_ok());
-            let _ = rooms.count(); // Performs a size assertion internally
-        }
-    }
-
-    #[test]
-    fn test_free_fails_unknown_room_id() {
-        let mut rooms = RoomBlock::new();
-
-        let renegade_room_id = RoomId::new();
-        assert!(rooms.free(renegade_room_id).is_err());
+    pub fn get_info(&self) -> Vec<RoomStatus> {
+        self.rooms.iter().map(|r| RoomStatus {
+            in_progress: false, // TODO: Get status from session state
+            room_name: r.name.to_owned(),
+            player_count: r.player_a.map_or_else(|| 0, |_| 1) + r.player_b.map_or_else(|| 0, |_| 1),
+        }).collect()
     }
 }
